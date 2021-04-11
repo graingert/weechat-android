a13ef3c3583f285a2cec232ea9f48cea69fd552a
oakkitten <oakkitten@users.noreply.github.com>
2021-04-11 16:52:13 +0100

    reimplement PingActionReceiver as Sync.PingAlarm

--- app/src/main/java/com/ubergeek42/WeechatAndroid/relay/BufferList.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/relay/BufferList.kt
@@ -7,7 +7,7 @@ import androidx.annotation.AnyThread
 import androidx.annotation.MainThread
 import androidx.annotation.WorkerThread
 import com.ubergeek42.WeechatAndroid.service.Events.SendMessageEvent
-import com.ubergeek42.WeechatAndroid.service.P
+import com.ubergeek42.WeechatAndroid.service.RelayService
 import com.ubergeek42.cats.Kitty
 import com.ubergeek42.cats.Root
 import com.ubergeek42.weechat.relay.protocol.Hdata
@@ -24,9 +24,9 @@ object BufferList {
 
     ////////////////////////////////////////////////////////////////////////////////////// lifecycle
 
-    @JvmStatic @WorkerThread fun onServiceAuthenticated() {
+    @JvmStatic @WorkerThread fun onServiceAuthenticated(service: RelayService) {
         defaultMessageHandlers.forEach { (id, handler) -> addMessageHandler(id, handler) }
-        syncManager.start()
+        syncManager.start(service)
     }
 
     @JvmStatic @AnyThread fun onServiceStopped() {
